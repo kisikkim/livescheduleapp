6d8344db02ed414718a951f3279a2e18fcadcdb2
Kisik Kim <kisik1978@yahoo.com>
2018-06-05 11:20:31 -0400

    clean up (#6)

--- lib/home/HomePage.dart
+++ lib/home/HomePage.dart
@@ -1,8 +1,8 @@
 import 'package:bidirectional_scroll_view/bidirectional_scroll_view.dart';
 import 'package:flutter/material.dart';
+import 'package:live_schdlue_app/datamodel/LiveProfileModel.dart';
 import 'package:live_schdlue_app/datamodel/StationData.dart';
 import 'package:live_schdlue_app/home/StationWidget.dart';
-import 'package:live_schdlue_app/datamodel/LiveProfileModel.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -47,23 +47,34 @@ class _MyHomePageState extends State<HomePage> {
     });
   }
 
-  Widget _listItemBuilder(num index, BuildContext context) {
-    StationData stationData = _stationDatas[index];
-    return new StationWidget(stationData);
-  }
-
   void _onClicked() {
     /**
      * N/A
      */
   }
 
-  StationData getRandomStation() {
+  StationData _getRandomStation() {
     final StationData stationData = _stationDatas[_stationIndex];
     _stationIndex++;
     if (_stationIndex >= _stationDatas.length) _stationIndex = 0;
 
     return stationData;
+  }
+
+  Column _stationColumn(List<Widget> list) {
+    return new Column(
+      children: list.map((widget) {
+        return widget;
+      }).toList(),
+    );
+  }
+
+  List<Column> _stationColumns(List<Widget> list) {
+    final List<Column> columns = <Column>[];
+    for (int i=0 ; i< 10; i++) {
+      columns.add(_stationColumn(list));
+    }
+    return columns;
   }
 
   @override
@@ -85,7 +96,7 @@ class _MyHomePageState extends State<HomePage> {
   Widget _buildWidgets() {
     List<Widget> list = new List();
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
       list.add(new Container(
         padding: new EdgeInsets.all(5.0),
         color: Colors.white,
@@ -93,84 +104,13 @@ class _MyHomePageState extends State<HomePage> {
         width: 200.0,
         child: new Container(
           color: Colors.grey,
-          child: new StationWidget(getRandomStation())
+          child: new StationWidget(_getRandomStation())
         ),
       ));
     }
 
     return new Row(
-      children: [
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-        new Column(
-          children: list.map((widget) {
-            return widget;
-          }).toList(),
-        ),
-      ],
+      children: _stationColumns(list),
     );
   }
 }
